fb4c2f62bd38e82d795057c8d19ddc1d9e488afc
Darryl <107661879+darrylljk@users.noreply.github.com>
2024-12-05 17:38:56 +0800

    Update About.py

--- About.py
+++ About.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='HR IntelliGen', page_icon='🐙')
+st.set_page_config(page_title='HR IntelliGen', page_icon='💫')
 st.header('IntelliGen HR')
 
 st.markdown("""
